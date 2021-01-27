2ee8f3d55e0e4bbecdb63dd605dad57329801b4e
Kris Gesling <kris.gesling@mycroft.ai>
2021-01-27 13:16:46 +0930

    Add long description and metadata for PyPI listing

--- setup.py
+++ setup.py
@@ -21,6 +21,9 @@
 # under the License.
 from setuptools import setup
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setup(
     name='pako',
     version='0.2.3',
@@ -33,10 +36,23 @@ setup(
     maintainer='Matthew Scholefield',
     maintainer_email='matthew331199@gmail.com',
     description='The universal package manager library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     entry_points={
         'console_scripts': {
             'pako=pako.__main__:main'
         }
     },
-    data_files=[('pako', ['LICENSE'])]
+    data_files=[('pako', ['LICENSE'])],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Software Distribution',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
 )
